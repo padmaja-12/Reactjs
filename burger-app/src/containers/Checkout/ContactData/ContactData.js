8fafa2e52012358ac437e77ff1e3e762827e6cc7
Padmaja Jha <jhajunu@gmail.com>
2020-05-20 19:43:35 +0530

    order action creator changes

--- burger-app/src/containers/Checkout/ContactData/ContactData.js
+++ burger-app/src/containers/Checkout/ContactData/ContactData.js
@@ -6,7 +6,8 @@ import Spinner from '../../../components/UI/Spinner/Spinner';
 import classes from './ContactData.module.css';
 import axios from '../../../axios-orders';
 import Input from '../../../components/UI/Input/Input';
-
+import withErrorHandler from '../../../hoc/withErrorHandler/withErrorHandler';
+import * as actions from '../../../store/actions/index';
 class ContactData extends Component {
     state = {
         orderForm: {
@@ -97,8 +98,7 @@ class ContactData extends Component {
     }
 
     orderHandler = ( event ) => {
-        event.preventDefault();
-        this.setState( { loading: true } );
+        event.preventDefault(); 
         const formData = {};
         for (let formElementIdentifier in this.state.orderForm) {
             formData[formElementIdentifier] = this.state.orderForm[formElementIdentifier].value;
@@ -108,15 +108,12 @@ class ContactData extends Component {
             price: this.props.price,
             orderData: formData
         }
-        axios.post( '/orders.json', order )
-            .then( response => {
-                this.setState( { loading: false } );
-                this.props.history.push( '/' );
-            } )
-            .catch( error => {
-                this.setState( { loading: false } );
-            } );
+        this.props.onOrderBurger(order);
     }
+
+    
+
+
 
     checkValidity(value, rules) {
         let isValid = true;
@@ -211,4 +208,10 @@ const mapStateToProps = state => {
     }
 };
 
-export default connect(mapStateToProps)(ContactData);
+const mapDispatchToProps = dispatch => {
+    return {
+      onOrderBurger : (orderData) => dispatch(actions.purchaseBurgerStart(orderData))
+    };
+};
+
+export default connect(mapStateToProps,mapDispatchToProps)(withErrorHandler(ContactData , axios));
