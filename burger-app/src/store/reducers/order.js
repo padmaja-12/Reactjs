b50d89b3b1511ce7beea53d0c306fa263cc0f41c
Padmaja Jha <jhajunu@gmail.com>
2020-05-20 23:10:46 +0530

    fetching order via redux

--- burger-app/src/store/reducers/order.js
+++ burger-app/src/store/reducers/order.js
@@ -12,12 +12,12 @@ const reducer = (state=initialState , action) => {
                return {
                   ...state,
                   purchased : false
-               }
+               };
            case actionTypes.PURCHASE_BURGER_START:
                return {
                  ...state,
                  loading:true
-               }
+               };
            case actionTypes.PURCHASE_BURGER_SUCCESS:
                  const newOrder = {
                      ...action.orderData,
@@ -34,6 +34,22 @@ const reducer = (state=initialState , action) => {
                    ...state,
                    loading : false 
                };
+            case actionTypes.FETCH_ORDERS_START:
+                return {
+                    ...state,
+                    loading : true
+                };
+            case actionTypes.FETCH_ORDERS_SUCCESS:
+                return {
+                   ...state,
+                   orders : action.orders,
+                   loading : false 
+                };
+            case actionTypes.FETCH_ORDERS_FAIL:
+                return {
+                  ...state,
+                  loading : false  
+                };
            default :
                return state;
        }
