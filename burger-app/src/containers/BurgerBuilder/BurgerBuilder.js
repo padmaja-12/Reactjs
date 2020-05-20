15c35a7fb3a44f3d64af6ebb0823b0c0e8cd0007
Padmaja Jha <jhajunu@gmail.com>
2020-05-20 21:52:56 +0530

    handling purchasing info

--- burger-app/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burger-app/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -8,7 +8,7 @@ import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
 import axios from '../../axios-orders';
 import Spinner from '../../components/UI/Spinner/Spinner';
 import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
-import* as burgerBuilderActions from '../../store/actions/index';
+import* as actions from '../../store/actions/index';
 
 class BurgerBuilder extends Component{
     state = {
@@ -35,6 +35,7 @@ purchaseCancelHandler = () => {
     this.setState({purchasing: false});
 }
 purchaseContinueHandler = () => {
+    this.props.onInitPurchase();
   this.props.history.push('/checkout');
 }
     render () {
@@ -90,10 +91,11 @@ const mapSateToProps = state => {
 
 const mapDispatchToProps = dispatch => {
     return {
-        onIngredientAdded : (ingName) => dispatch(burgerBuilderActions.addIngredient(ingName)) ,
-        onIngredientRemoved : (ingName) => dispatch(burgerBuilderActions.removeIngredient(ingName)) ,
-        onInitIngredients : () => dispatch(burgerBuilderActions.initIngredients())
+        onIngredientAdded : (ingName) => dispatch(actions.addIngredient(ingName)) ,
+        onIngredientRemoved : (ingName) => dispatch(actions.removeIngredient(ingName)) ,
+        onInitIngredients : () => dispatch(actions.initIngredients()),
+        onInitPurchase : () => dispatch(actions.purchaseInit())
     };
-}
+};
 
 export default connect(mapSateToProps,mapDispatchToProps)(withErrorHandler(BurgerBuilder,axios));
--- burger-app/src/containers/Checkout/Checkout.js
+++ burger-app/src/containers/Checkout/Checkout.js
@@ -3,7 +3,14 @@ import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSumm
 import {Route, Redirect} from 'react-router-dom';
 import { connect } from 'react-redux';
 import ContactData from './ContactData/ContactData';
+
 class Checkout extends Component {
+
+    componentDidMount () {
+       this.props.onInitPurchase();
+    }
+
+
    checkoutCancelledHandler = () => {
        this.props.history.goBack();
     }
@@ -16,8 +23,10 @@ class Checkout extends Component {
             let summary = <Redirect to = "/"/>
             
             if(this.props.ings) {
+                const purchasedRedirect = this.props.purchased ? <Redirect to = "/" /> : null;
                 summary = (
-                    <div>         
+                    <div>  
+                           {purchasedRedirect}       
                           <CheckoutSummary 
                           ingredients={this.props.ings}
                           checkoutCancelled={this.checkoutCancelledHandler}
@@ -35,7 +44,11 @@ class Checkout extends Component {
 
 const mapStateToProps = state => {
     return {
-        ings : state.burgerBuilder.ingredients
+        ings : state.burgerBuilder.ingredients,
+        purchased : state.burgerBuilder.purchased
     };
-}
+};
+
+
+
 export default connect(mapStateToProps)(Checkout);
